3f64f0be728b29bcb4172cf83d6e8dd3cf134ab5
Ada Udoji <ada.udoji@centria.fi>
2020-12-02 12:14:13 +0200

    1.12.2020

--- Exercises/Part 2/Exercise 060/Program.cs
+++ Exercises/Part 2/Exercise 060/Program.cs
@@ -6,18 +6,23 @@ namespace exercise_60
   {
         public static void Main(String[] args)
     {
-
+            PrintStars();
     }
 
 
     public static void PrintStars(int number)
-    {
+    { 
       // you can print one star with the command
       // Console.Write("*");
       // call the print command n times
       // in the end print a line break with the comand
       // Console.WriteLine("");
-    }
+       int i = 0;
+        while (i < number) 
+         {
+       Console.WriteLine("*");
+                i++;
+        }
 
     public static void PrintSquare(int size)
     {
