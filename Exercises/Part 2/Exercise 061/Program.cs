843cb06625a9391b279c73cfec244832a69f4edb
Ada Udoji <ada.udoji@centria.fi>
2020-12-02 13:08:34 +0200

    2.12.2020

--- Exercises/Part 2/Exercise 061/Program.cs
+++ Exercises/Part 2/Exercise 061/Program.cs
@@ -5,28 +5,61 @@ namespace exercise_61
   class Program
   {
     public static void Main(String[] args)
-    {
-
+     {  
+       PrintSpaces(1);
+            Console.WriteLine("");
+            PrintRightTriangle(4);
+            Console.WriteLine("");
+            ChristmasTree(4);
     }
 
-    public static void PrintStars(int number)
-    {
+       public static void PrintSpaces(int number)
+        {
+          
+            int i = 0;
+         while (i < number) 
+          {
+             Console.Write("");
+             i++;
+           
+          }
+       
+         }
 
-    }
+    //public static void PrintSpaces(int number)
+    //{
 
-    public static void PrintSpaces(int number)
-    {
-
-    }
+    //}
 
     public static void PrintRightTriangle(int size)
     {
-
+         
+           for (int i = 1; i <= size; i++)
+            {
+              for (int j = i; j <= size; j++)
+                {
+                Console.Write("*");
+                }
+              Console.WriteLine();
+            }
     }
 
     public static void ChristmasTree(int height)
     {
-
-    }
+         for (int i = 0; i <= height; i++)
+         {
+              for (int j = i; j <= height - 1; j++)
+              {
+                Console.Write(" ");
+              }
+              for (int k = 0; k <= i; k++)
+               {
+                Console.Write("* ");
+               }   
+               
+                Console.WriteLine();
+      
+          }
+          Console.ReadKey();
   }
 }
