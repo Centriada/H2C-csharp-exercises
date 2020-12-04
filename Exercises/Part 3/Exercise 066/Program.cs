e8421488e7613bd5270a4052d4640a55b1914e0e
Ada Udoji <ada.udoji@centria.fi>
2020-12-04 13:40:41 +0200

    4.12.2020

--- Exercises/Part 3/Exercise 066/Program.cs
+++ Exercises/Part 3/Exercise 066/Program.cs
@@ -8,16 +8,35 @@ namespace exercise_66
     public static void Main(string[] args)
     {
       List<string> list = new List<string>();
+           
+
       while (true)
-      {
+      { 
         string input = Console.ReadLine();
+                                                             
         if (input == "")
         {
+             int index = 0;
+            if (index < list.Count) 
+             {
+                            index = index +1;
+                   Console.WriteLine(list[index - 1]); 
+               
+             }  
           break;
-        }
+              
         list.Add(input);
-      }
-
+        
+            
+                       
+         }  
+        
+      }      
+      
+       
+           
+       
+        
     }
   }
 }
