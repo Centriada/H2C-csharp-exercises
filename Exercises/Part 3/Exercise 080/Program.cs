4b8dd46724b3d792fd00cce7fc995d559a3a6d38
Ada Udoji <ada.udoji@centria.fi>
2020-12-07 21:10:38 +0200

    7.12.2020

--- Exercises/Part 3/Exercise 080/Program.cs
+++ Exercises/Part 3/Exercise 080/Program.cs
@@ -14,8 +14,10 @@ namespace exercise_80
     }
 
     public static int SumOfNumbersInArray(int[] numbers) {
-
-      return 0;
+     int sum = 0;
+            foreach (int number in numbers)
+                sum+=number;
+      return sum;
     }
 
   }
