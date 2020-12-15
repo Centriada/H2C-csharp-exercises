98956701218b75b7a04ac520c51fb9e0f8c0b755
Ada Udoji <ada.udoji@centria.fi>
2020-12-15 12:42:22 +0200

    15.12.2020

--- Exercises/Part 3/Exercise 076/Program.cs
+++ Exercises/Part 3/Exercise 076/Program.cs
@@ -8,7 +8,31 @@ namespace exercise_76
     public static void Main(string[] args)
     {
 
+        List<int> numbers = new List<int>();
+        numbers.Add(3);
+        numbers.Add(2);
+        numbers.Add(6);
+        numbers.Add(-1);
+        Console.WriteLine(Sum(numbers));
+         Sum(numbers);
+        numbers.Add(5);
+        numbers.Add(1);
+      
+        Console.WriteLine(Sum(numbers));
     }
+       
+    public static int Sum(List<int> numbers)
+    {
+         int total = 0;        
+          foreach (int number in numbers)
+          {
+           total = total + number; 
+                
+         }
+          return total;
+               
+            
+   }
 
   }
 }
