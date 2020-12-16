a9781bc16f5ee1ccbcc911b2dcb9f84d5e2ffc96
Ada Udoji <ada.udoji@centria.fi>
2020-12-16 22:49:40 +0200

    16.12.2020

--- Exercises/Part 3/Exercise 090/Program.cs
+++ Exercises/Part 3/Exercise 090/Program.cs
@@ -7,9 +7,37 @@ namespace exercise_90
   {
     public static void Main(string[] args)
     {
-
-
-    }
+         int ages;
+      int highestAge = 0;
+       int  lowestAge = 0;
+        Console.WriteLine("Please give name and age");
+            string input;
+        while (true)
+        {
+            input = Console.ReadLine();
+          var words = input.Split(',');
+               
+          if (input != "")
+            {
+                    
+           } for (int i = 0; i < input.Length; i++)
+              {   
+                     ages = Convert.ToInt32(words[1]);
+               if (ages < lowestAge) lowestAge = ages;
+               if(ages > highestAge) highestAge = ages;
+             }    
+              
+               if (input =="")
+                    {
+                break;
+                }
+               Console.WriteLine("Age of the oldest is : " + highestAge);
+        }       
+             
+                 
+            
+       
+     }
   }
 }
 
