fd9c47ec760fe2be6753c48be585360f8109186b
Ada Udoji <ada.udoji@centria.fi>
2020-12-16 15:43:09 +0200

    16.12.2020

--- Exercises/Part 3/Exercise 089/Program.cs
+++ Exercises/Part 3/Exercise 089/Program.cs
@@ -7,8 +7,22 @@ namespace exercise_89
   {
     public static void Main(string[] args)
     {
-
-
+     while(true)
+     {
+               string sentance = Console.ReadLine();
+                var words = sentance.Split(' ');
+       for (int i = 0; i < words.Length; i++)
+       {
+         if (sentance != "")
+         {
+              Console.WriteLine(words[words.Length - 1]);    
+               break;
+         }
+          
+             
+          
+       }
+     }
     }
   }
 }
