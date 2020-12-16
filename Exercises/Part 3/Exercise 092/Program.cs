d0bd63d1996efb72762425d15e252b3cd2910d0f
Ada Udoji <ada.udoji@centria.fi>
2020-12-17 00:45:31 +0200

    17.12.2020

--- Exercises/Part 3/Exercise 092/Program.cs
+++ Exercises/Part 3/Exercise 092/Program.cs
@@ -7,7 +7,38 @@ namespace exercise_92
   {
     public static void Main(string[] args)
     {
-
+          int highest = 0;
+          int longest = 0;
+          string longestName = "";
+      Console. WriteLine("Please enter name and birth year:");
+      while (true)
+      {
+            string input = Console.ReadLine();
+        
+               
+          if (input != "")
+          {
+               var details = input.Split(',');
+                int birthYear = Convert.ToInt32(details[1]);
+                int nameLength = details[0].Length;
+                int age = 2020 - (birthYear);
+            if (age > highest) highest = age;
+            if (nameLength > longest)
+            {
+               longest = nameLength;
+               longestName = details[0];
+            }
+           }
+            
+           else
+            {
+                     Console.WriteLine("The longest name is: " + longestName);
+                    Console.WriteLine("The highest age is: " + highest); 
+            break;
+            }
+           
+          
+      }
     }
   }
 }
