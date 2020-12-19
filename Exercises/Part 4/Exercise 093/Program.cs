43c11522a801d89e43e9eb6bc284b70fc2b943b5
Ada Udoji <ada.udoji@centria.fi>
2020-12-19 18:35:12 +0200

    19.12.2020

--- Exercises/Part 4/Exercise 093/Program.cs
+++ Exercises/Part 4/Exercise 093/Program.cs
@@ -8,7 +8,15 @@ namespace exercise_93
     {
       // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
+       
 
+      Account heikkisAccount = new Account("Heikki's account", 100.00);
+      Account heikkisSwissAccount = new Account("Heikki's account in Switzerland", 1000000.00);
+            Console.WriteLine("Intial state");
+            Console.WriteLine(heikkisAccount);
+            Console.WriteLine(heikkisSwissAccount);
+            heikkisAccount.Deposit(20);
+            Console.WriteLine("The balance of Heikki's account is now: " + heikkisAccount.balance);
     }
   }
 }
---
+++ Exercises/Part 4/Exercise 093/Transaction.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace exercise_93
+{
+    class Transaction
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
